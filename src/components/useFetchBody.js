f4a2e56fc59c26f4a85b665b9ae2f043f6bf480a
Yas <yashwantyadav2003@gmail.com>
2024-03-16 10:56:57 +0530

    CORS fix

--- src/components/useFetchBody.js
+++ src/components/useFetchBody.js
@@ -5,7 +5,7 @@ import { generateProxyUrl } from "../utils/constants";
 const useFetchBody=()=>{
 
     const[resData,setResData]=useState(null);
-    const resource= "https://corsproxy.org/?"+ encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.586855&lng=88.290717&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+   const resource = generateProxyUrl("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.586855&lng=88.290717&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
     
     useEffect(()=>{
         fetchData();
@@ -13,7 +13,7 @@ const useFetchBody=()=>{
     },[]);
 
     const fetchData=async()=>{
-        const data= await fetch("https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data= await fetch(resource);
 
         const json= await data.json();
         // setListOfRestraunt(json?.data?.cards[4]?.card?.card.gridElements?.infoWithStyle?.restaurants);
